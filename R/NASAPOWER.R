6e9a7c4cc030b6cb4b6844c7140f5e0fdcfbf97e
Leo Crespo <leonardoabdiel@gmail.com>
2020-12-16 16:14:25 -0700

    Initial commit

--- R/NASAPOWER.R
+++ R/NASAPOWER.R
@@ -10,20 +10,28 @@
 #' Download NASA POWER data
 #'
 #' @description  Downloads climate data from the NASA POWER data base. This functions requires the nasapower library to be installed
-#' @param t Is a dataframe with longitud and latitude in decimal formats and a "Loc_no" column as location ID.
+#' @param t Is a dataframe with longitud and latitude in decimal formats and a location ID.
 #' @param lon Is the column index in t that contains the longitud in decimal format
 #' @param lat Is the column index in t that contains the latitude in decimal format
-#' @param new.names Is a vector of new names for the variables to be extracted. Used for simplicity in naming the variables.
+#' @param loc.id Is column name of the location ids in the dataframe t
+#' @param set.newnames Logical indicating if the meteorological variables will be renamed. Default is TRUE. Set to FALSE if a different set
+#' of variables will be downloaded. FALSE will keep the names as in NASA POWER
 #' @param ... Arguments passed to the get_power function
 #' @export
 
-climdata = function(t = NULL, lon = NULL, lat = NULL, new.names = NULL, ... ){
+climdata = function(t = NULL, lon = NULL, lat = NULL, loc.id = NULL, set.newnames = T, ... ){
 
-#library(nasapower)
+t$Loc_no = t[, loc.id]
 t.split= split(t, t$Loc_no)
 t.split = lapply(t.split, droplevels)
-#lon & lat must indicate column index where longitud and latitud are in the dataframe
-d = lapply(t.split, function(x) nasapower::get_power(lonlat = c(x[,lon], x[,lat]),...))
+new.names = c("t.av", "t.min", "t.max",
+              "rel.h", "rad", "dew.p", "pp",
+              "cdd", "clrsky", "wind.speed")
+
+d = lapply(t.split, function(x) nasapower::get_power(lonlat = c(x[,lon], x[,lat]), temporal_average = "DAILY",
+                                                     pars=c("T2M","T2M_MIN", "T2M_MAX","RH2M",
+                                                            "ALLSKY_SFC_SW_DWN", "T2MDEW", "PRECTOT",
+                                                            "CDD0", "CLRSKY_SFC_SW_DWN", "WS2M"), ...))
 
 d.d = reshape2::melt(d, id = c("LON", "LAT"))
 
@@ -33,14 +41,15 @@ tmp <- plyr::ddply(d.d, plyr::.(L1, variable), transform, newid = paste(L1, seq_
 df = reshape2::dcast(tmp, L1 + LAT + LON + newid ~ variable, value.var = "value")
 df = df[,-which(colnames(df) == "newid")]
 
-#library(zoo)
 df$YYYYMMDD = zoo::as.Date(df$YYYYMMDD)
 
 df = df[order(df$L1, df$YEAR, df$MM, df$DD),]
-colnames(df)[1] = "Loc_no"
+colnames(df)[1] = loc.id
 colnames(df)[8] = "Date"
-#the new.names vector should be the new names of the climate variables
+
+if(set.newnames == T){
 colnames(df)[9:ncol(df)] = new.names
+}
 
 df = merge(x = df, y = t, by = "Loc_no")
 df
@@ -59,42 +68,69 @@ df
 #                                                          #
 ############################################################
 
+
+
+
 #' Summarize climdata outputs
 #'
 #' @description Function to summarize the climate date by groups of days for a single location. It requires the locs.merged.climate object which is the climate data.
 #' Dates must be in the appropriate format otherwise errors are shown.
-#' @param df Is a one row (one location) data frame with the date of sowing (sowing.d), or a date from which the variables will start to be summarized.
-#' @param locs.merged.climate Is a list of locations with the climate data
-#' @param days Is the number of days after the initial date that will be consider to extract the data
-#' @param last.day Is the number of days that will be used to average the data. i.e, las.day must be <= than days and multiple of days
+#' @param df A dataframe of one row (one location) that contains the date from which meteorological variables (MVs) will be extracted.
+#' @param climate.data A list of or dataframe of MVs.
+#' @param total.days The number of days from which the MVs will be extracted. Is the number of days after the initial date that will be consider to extract the data.
+#' @param by.periods The number of days which the MVs will be averaged over. i.e, it must be multiple and <= than total.days.
+#' @param start.date The column name in df that contains the date from which the extraction will start
+#' @param loc.id The column name in df that contains the location ids
+#' @param Date The column name in climate.data that contains the date of the recorded MVs
+#' @param mv.names The index of the columns that contain the MVs in climate.data
+#' @param pp.name The name of the column that contains the rainfall. This is used since percipitation is summed over the by.periods, instead of averaged as the other MVs
+#'
+#' @return
 #' @export
+#'
 
-periodicextraction = function(df = NULL, locs.merged.climate = NULL,
-                               days=120, last.day=120){
-    period = days-1 # "last.day" must be multiple of "days"
-    df$date.covs = df$sowing.d + period
-    locno = as.character(df$Loc_no)
-    tmp = locs.merged.climate[[locno]]
-    period.covs = subset(tmp, subset = Date >= df$sowing.d
-                         & Date <= df$date.covs
-                         & Loc_no == df$Loc_no )
-    period.covs
+periodicextraction <- function(df = NULL, climate.data = NULL,
+                               total.days = 120,
+                               by.periods = 10,
+                               start.date = "sowing.d",
+                               loc.id = "Loc_no",
+                               Date = "Date",
+                               mv.names = 9:18,
+                               pp.name = "pp") {
+
+
+    period = total.days - 1
+    df$start.date = df[,start.date]
+    df$date.covs = df$start.date + period
+    loc.no = as.character(df[, loc.id])
+    tmp = climate.data[[loc.no]]
+    tmp$Loc_no = df[,loc.id]
+    tmp$Date = tmp[, Date]
+
+
+    period.covs = subset(x = tmp, subset = tmp$Date >= df$start.date & Date <= df$date.covs & tmp$Loc_no == loc.no)
     period.covs = droplevels(period.covs)
-    group.means = rep(1:(days/last.day), each=last.day)
+    group.means = rep(1:(total.days/by.periods), each = by.periods)
     period.covs = cbind(period.covs, group.means)
-    #library(doBy)
-    cov.means = doBy::summaryBy(rad + t.av + t.min + t.max + rel.h + dew.p + cdd
-                          + clrsky + wind.speed
-                          ~ group.means, data=period.covs, FUN = mean)
-    cov.pp.sum = doBy::summaryBy(pp ~ group.means, data = period.covs, FUN = sum)
-    f.env.covs = cbind(cov.means, cov.pp.sum$pp.sum)
-    colnames(f.env.covs)[11] = "pp"
-    #library(reshape2)
-    f.conv.melt = reshape2::melt(f.env.covs, id.vars = "group.means")
-    conv.cast = reshape2::dcast(f.conv.melt, 1 ~ variable+group.means)
-    loc.env.covariables = cbind(df, conv.cast)
-    loc.env.covariables = loc.env.covariables[, -c(15)]
-    loc.env.covariables
 
+    mvs = base::colnames(period.covs)[mv.names]
+    mvs = mvs[!mvs %in% pp.name]
+    frm = base::paste(mvs, collapse = " + ")
+    frm = base::paste(frm, "group.means", sep = "~")
+    f.mvs = stats::as.formula(frm)
+
+    cov.means = doBy::summaryBy(formula = f.mvs, data = period.covs, FUN = mean, keep.names = TRUE)
+
+    p.form = paste(pp.name, "group.means", sep = "~")
+    p.form = stats::as.formula(p.form)
+
+    cov.pp.sum = doBy::summaryBy(formula = p.form, data = period.covs,
+                                 FUN = sum, keep.names = TRUE)
+    f.env.covs = cbind(cov.means, cov.pp.sum[,2])
+    colnames(f.env.covs)[11] = "pp"
+    f.conv.melt = reshape2::melt(f.env.covs, id.vars = "group.means")
+    conv.cast = reshape2::dcast(f.conv.melt, 1 ~ variable + group.means)
+    loc.env.covariables = cbind(df, conv.cast[, 2:ncol(conv.cast)])
+    loc.env.covariables
 
 }
